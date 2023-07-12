7f23e4ced138f94ef2cb7cc5f0b6144041f1e0f6
zaingova <zain.gova.school@gmail.com>
2023-07-12 14:20:33 -0400

    added imports and empty functions for component/page files

---
+++ client/src/pages/Projects.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import ProjectList from '../components/ProjectList';
+
+function Projects() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Projects;
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+// import { SIGNUP } from '../utils/mutations';
+// import Auth from '../utils/auth';
+
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+
+function Signup(props) {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Signup;
