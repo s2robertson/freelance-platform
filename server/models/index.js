9b31978d5ca7c2cb7e1479ce34332170e1c2cad8
saveetha17 <saveethakathiravan@gmail.com>
2023-07-13 10:23:15 -0400

    Worked on Index in Model

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require("./User");
+const Project = require("./Project");
+const Service = require("./Service");
+const Message = require("./Message");
+
+// Exporting the models
+module.exports = { User, Project, Service, Message };
