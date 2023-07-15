6d5d02521ed8ccd66d62c715f4bf4cee4cf66dfb
zaingova <zain.gova.school@gmail.com>
2023-07-15 17:59:36 -0400

    adjusted mutations in client-side to reflect back-end changes to resolvers/typedefs

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,91 +4,146 @@ export const LOGIN = gql`
   mutation Login($email: String!, $password: String!) {
   login(email: $email, password: $password) {
     token
-    user {
-      _id
-    }
   }
 }
 `
 
 export const ADD_USER = gql`
-  mutation AddUser($username: String!, $email: String!, $password: String!, $isEmployer: Boolean!, $skills: [ServiceInput]) {
-    addUser(username: $username, email: $email, password: $password, isEmployer: $isEmployer, skills: $skills) {
-      token
-      user {
-        _id
-      }
+  mutation AddUser($username: String!, $email: String!, $password: String!, $isEmployer: Boolean!, $profileDescription: String) {
+  addUser(username: $username, email: $email, password: $password, isEmployer: $isEmployer, profileDescription: $profileDescription) {
+    user {
+      _id
+      username
+      email
+      isEmployer
+      profileDescription
     }
   }
+}
 `
 
 export const UPDATE_USER = gql`
-  mutation UpdateUser($id: ID!, $username: String, $email: String, $password: String, $isEmployer: Boolean!, $skills: [ServiceInput]) {
-    updateUser(_id: $id, username: $username, email: $email, password: $password, isEmployer: $isEmployer, skills: $skills) {
+  mutation UpdateUser($username: String, $email: String, $password: String, $isEmployer: Boolean, $profileDescription: String) {
+  updateUser(username: $username, email: $email, password: $password, isEmployer: $isEmployer, profileDescription: $profileDescription) {
+    _id
+    username
+    email
+    isEmployer
+    profileDescription
+    skills {
       _id
+      name
+    }
+    projects {
+      _id
+      name
+    }
+    messages {
+      _id
+      text
     }
   }
+}
 `
 
 export const ADD_PROJECT = gql`
-  mutation AddProject($name: String!, $description: String!, $ownerId: ID!, $freelancers: [UserInput], $dueDate: String, $budget: Int, $services: [ServiceInput]) {
-    addProject(name: $name, description: $description, ownerId: $ownerId, freelancers: $freelancers, dueDate: $dueDate, budget: $budget, services: $services) {
+ mutation AddProject($name: String!, $description: String!, $freelancers: [ID], $dueDate: String, $budget: Int, $services: [ID]) {
+  addProject(name: $name, description: $description, freelancers: $freelancers, dueDate: $dueDate, budget: $budget, services: $services) {
+    _id
+    name
+    description
+    owner {
       _id
+      username
     }
-  }
-`
-
-export const UPDATE_PROJECT = gql`
-  mutation UpdateProject($projectId: ID!, $name: String!, $description: String!, $ownerId: ID!, $freelancers: [UserInput], $dueDate: String, $budget: Int, $services: [ServiceInput]) {
-    updateProject(projectId: $projectId, name: $name, description: $description, ownerId: $ownerId, freelancers: $freelancers, dueDate: $dueDate, budget: $budget, services: $services) {
+    freelancers {
       _id
+      username
     }
-  }
-`
-
-export const DELETE_PROJECT = gql`
-  mutation DeleteProject($projectId: ID!) {
-    deleteProject(projectId: $projectId)
-  }
-`
-
-export const ADD_SERVICE = gql`
-  mutation AddService($name: String!) {
-    addService(name: $name) {
-      _id
-    }
-  } 
-`
-
-export const UPDATE_SERVICE = gql`
-  mutation UpdateService($serviceId: ID!, $name: String) {
-    updateService(serviceId: $serviceId, name: $name) {
+    dueDate
+    budget
+    servicesNeeded {
       _id
       name
     }
   }
+}
+`
+
+export const UPDATE_PROJECT = gql`
+  mutation UpdateProject($id: ID!, $name: String, $description: String, $freelancers: [ID], $dueDate: String, $budget: Int, $services: [ID]) {
+  updateProject(_id: $id, name: $name, description: $description, freelancers: $freelancers, dueDate: $dueDate, budget: $budget, services: $services) {
+    _id
+    name
+    description
+    owner {
+      _id
+      username
+    }
+    freelancers {
+      _id
+      username
+    }
+    dueDate
+    budget
+    servicesNeeded {
+      _id
+      name
+    }
+  }
+}
+`
+
+export const DELETE_PROJECT = gql`
+  mutation DeleteProject($projectId: ID!) {
+  deleteProject(projectId: $projectId)
+}
+`
+
+export const ADD_SERVICE = gql`
+  mutation AddService($name: String!) {
+  addService(name: $name) {
+    _id
+    name
+  }
+}
+`
+
+export const UPDATE_SERVICE = gql`
+  mutation UpdateService($id: ID, $name: String) {
+  updateService(_id: $id, name: $name) {
+    _id
+    name
+  }
+}
 `
 
 export const DELETE_SERVICE = gql`
-  mutation DeleteService($serviceId: ID!) {
-    deleteService(serviceId: $serviceId)
-  }
+  mutation UpdateService($serviceId: ID!) {
+  deleteService(serviceId: $serviceId)
+}
 `
 
 export const SEND_MESSAGE = gql`
-  mutation SendMessage($text: String!, $senderId: UserInput!, $dateSent: String!, $receiverIds: [UserInput]!) {
-    sendMessage(text: $text, senderId: $senderId, dateSent: $dateSent, receiverIds: $receiverIds) {
+  mutation SendMessage($text: String!, $receiverIds: [ID]!) {
+  sendMessage(text: $text, receiverIds: $receiverIds) {
+    _id
+    text
+    sender {
       _id
-      text
-      sender {
-        _id
-        name
-      }
-      receiver {
-        _id
-        name
-      }
-      dateSent
+      username
     }
+    receiver {
+      _id
+      username
+    }
+    dateSent
   }
+}
+`
+
+export const DELETE_MESSAGE = gql`
+  mutation SendMessage($messageId: ID!) {
+  deleteMessage(messageId: $messageId)
+}
 `
