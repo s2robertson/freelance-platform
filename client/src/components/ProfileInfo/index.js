0d4b372c87a6b9080f5a829a0cb7323360272445
Steven Robertson <s2robertson@gmail.com>
2023-07-14 20:37:14 -0400

    created message form and added it to profile if logged in

--- client/src/components/ProfileInfo/index.js
+++ client/src/components/ProfileInfo/index.js
@@ -1,8 +1,54 @@
-import React from "react";
+import { useState } from "react";
 
-export default function ProfileInfo() {
-  return (
-    <>
-    </>
-  )
+import MessageForm from "../MessageForm";
+import { loggedIn } from "../../utils/auth";
+
+function ProfileInfo({ user, startEdit = null }) {
+    const [showMessageForm, setShowMessageForm] = useState(false);
+
+    let editButton = null;
+    let messageButton = null;
+    if (startEdit) {
+        editButton = (
+            <button
+                onClick={startEdit}
+                className="border-solid border-2 p-1"
+            >
+                Edit
+            </button>
+        )
+    } else if (loggedIn()) {
+        messageButton = (
+            <button
+                onClick={() => setShowMessageForm(true)}
+                className="border-solid border-2 p-1"
+            >
+                Message this user
+            </button>
+        )
+    }
+
+    return (
+        <div className="mb-2">
+            <h2 className="text-2xl font-bold">{user.username}</h2>
+            {editButton}
+            {messageButton}
+            <p>{user.profileDescription}</p>
+            {user.skills && user.skills.length > 0 ? (
+                <>
+                    <h3>Skills:</h3>
+                    <ul className="list-disc">
+                        {user.skills.map(skill => (
+                            <li key={skill._id}>{skill.name}</li>
+                        ))}
+                    </ul>
+                </>
+            ) : null}
+            {showMessageForm ? (
+                <MessageForm receiver={user} />
+            ) : null}
+        </div>
+    )
 }
+
+export default ProfileInfo;
