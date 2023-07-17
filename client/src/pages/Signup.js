d42eb01b0e395605c1109bba0861503c6c7ae7b4
saveetha17 <saveethakathiravan@gmail.com>
2023-07-17 17:46:40 -0400

    footer removed from signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,7 +3,6 @@ import { useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
 import { ADD_USER, LOGIN } from "../utils/mutations";
 import { setToken } from "../utils/auth";
-import { Footer } from "../components/Footer";
 
 import Nav from "../components/Nav";
 
@@ -187,7 +186,6 @@ function Signup(props) {
           </div>
         </form>
       </div>
-      <Footer />
     </>
   );
 }
